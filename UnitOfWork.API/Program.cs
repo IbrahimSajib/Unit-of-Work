bf7fd3967ed12c45acd6beb0b31d7e7323202b04
IbrahimSajib <ibrahimsajib.bd@gmail.com>
2025-08-15 11:00:31 +0600

    Dependency Injection Added

--- UnitOfWork.API/Program.cs
+++ UnitOfWork.API/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using UnitOfWork.Core.Entities;
+using UnitOfWork.Core.Interfaces;
+using UnitOfWork.Infrastructure.Data;
+using UnitOfWork.Infrastructure.Repositories;
+using UnitOfWork.Services.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// EF Core with In-Memory DB
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseInMemoryDatabase("UnitOfWorkDb"));
+
+// Dependency Injection
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork.Infrastructure.Repositories.UnitOfWork>();
+builder.Services.AddScoped<UserService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
