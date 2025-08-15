655b26b53edc263e3991e25af67a51dc4fb30dc5
IbrahimSajib <ibrahimsajib.bd@gmail.com>
2025-08-15 10:36:13 +0600

    UnitOfWork Added

---
+++ UnitOfWork.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using UnitOfWork.Core.Entities;
+
+namespace UnitOfWork.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<User> Users { get; }
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ UnitOfWork.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using UnitOfWork.Core.Entities;
+using UnitOfWork.Core.Interfaces;
+using UnitOfWork.Infrastructure.Data;
+
+namespace UnitOfWork.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _db;
+        private IGenericRepository<User>? _userRepository;
+
+        public UnitOfWork(DataContext db)
+        {
+            _db = db;
+        }
+
+        public IGenericRepository<User> Users => _userRepository ??= new GenericRepository<User>(_db);
+
+        public async Task<int> SaveChangesAsync() => await _db.SaveChangesAsync();
+
+        public void Dispose() => _db.Dispose();
+
+    }
+}
