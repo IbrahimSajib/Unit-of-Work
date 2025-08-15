4f7488f84ca4835db06a316719a920446ca0d9a7
IbrahimSajib <ibrahimsajib.bd@gmail.com>
2025-08-15 10:18:06 +0600

    User Entitie Added

---
+++ UnitOfWork.Core/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitOfWork.Core.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+    }
+}
